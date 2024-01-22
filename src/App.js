6f67a8ad4a0a253dba9d0c8fa1e2647e021dd589
D2icehacker <d2ice00@gmail.com>
2024-01-22 14:45:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { Component } from "react"
-import Navbar from "./component/Navbar"
-import Hero from "./component/Hero"
-import Card from "./component/Card"
-import data from "./data"
-import Footer from "./component/Footer"
+import React from "react"
+import Header from "./component/Header"
+import Meme from "./component/Meme"
+
+// CSS
+import "./index.css"
 
 
 export default function App() {
-
-  const cards = data.map(item => {
-    return (
-      <Card
-        key={item.id}
-        {...item}
-      />
-    )
-  })
-
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <section className="cards-list">
-        {cards}
-      </section>
-      <Footer />
-    </div>
+    <>
+      <Header />
+      <Meme />
+    </>
+
   )
 }
--- src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-        <p>🏡 Airbnb clone 2024 by (D2ice) 🚀</p>
-      </footer>
-    )
-}
